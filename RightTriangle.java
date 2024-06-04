56f02171d4af0a46de089a5b1672bc60cfb93f5c
Romy-James-P <romyjamesp@gmail.com>
2024-06-04 15:38:29 +0530

    feat:first

---
+++ RightTriangle.java
@@ -0,0 +1,18 @@
+package com.ilpbatch4.utility;
+
+public class RightTriangle {
+
+	public static void main(String[] args) {
+		int n=5;
+		printRightTriangle(n);
+	}
+	private static void printRightTriangle(int n) {
+		for(int i=0;i<n;i++) {
+			for {int j=0;j<i;j++){
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+	}
+
+}
